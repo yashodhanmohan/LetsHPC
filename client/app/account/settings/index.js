99a2f67504f6c8750774186f606d98265b99b5ba
Yashodhan Mohan Bhatnagar <yashodhan.mohan@gmail.com>
2016-12-06 18:36:45 +0530

    First commit

---
+++ client/app/account/settings/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import angular from 'angular';
+import SettingsController from './settings.controller';
+
+export default angular.module('yashwantProjectApp.settings', [])
+  .controller('SettingsController', SettingsController)
+  .name;
