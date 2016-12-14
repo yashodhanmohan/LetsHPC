7e4ce28e2d4921c4f69bcb5a8ecfdca11021407b
yashwantkeswani <yashwant.keswani@gmail.com>
2016-12-14 18:35:55 +0530

    Bugfix

--- server/api/apmap/index.js
+++ server/api/apmap/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./cpmap.controller');
+var controller = require('./apmap.controller');
 
 var router = express.Router();
 
