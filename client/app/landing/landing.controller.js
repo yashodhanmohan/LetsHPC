0107a0ef09289795d75b4fae08650556b7366ea9
Yashodhan Mohan Bhatnagar <yashodhan.mohan@gmail.com>
2017-01-14 16:46:14 +0530

    LAnding page changes. Footer changes. Navbar changes.

--- client/app/landing/landing.controller.js
+++ client/app/landing/landing.controller.js
@@ -5,6 +5,21 @@ export default class LandingController {
     /*@ngInject*/
     constructor($http) {
         console.log('Landing Controller');
+        $(function() {
+            $('a[href*="#"]:not([href="#"])').click(function() {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    console.log(target);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000);
+                        return false;
+                    }
+                }
+            });
+        });
     }
 
 }
