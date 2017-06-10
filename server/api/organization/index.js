6e2f2a22d79c370a361f60c2e8ff56134f865d55
Yashodhan Mohan Bhatnagar <yashodhan.mohan@gmail.com>
2017-06-10 17:06:57 +0530

    Added organization model and controller

---
+++ server/api/organization/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let express = require('express');
+let controller = require('./organization.controller');
+
+let router = express.Router();
+
+router.get('/', controller.index);
+router.get('/:id', controller.show);
+router.post('/', controller.create);
+router.put('/:id', controller.upsert);
+router.patch('/:id', controller.patch);
+router.delete('/:id', controller.destroy);
+
+module.exports = router;
---
+++ server/api/organization/organization.model.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+var OrganizationSchema = new mongoose.Schema({
+    name: String,
+    country: String
+});
+
+export default mongoose.model('Category', CategorySchema);
