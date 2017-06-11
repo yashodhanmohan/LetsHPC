18612a74b8b8d1b8551704d846edbfe3676bfb60
Yashodhan Mohan Bhatnagar <yashodhan.mohan@gmail.com>
2017-06-11 14:54:56 +0530

    Added "Show all users" route

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -5,6 +5,7 @@ let controller = require('./user.controller');
 
 let router = express.Router();
 
+router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.upsert);
